dce56b5103d49c16f9088676ea422a5235e423fb
Maxime Cottin <maximecottin@Maximes-MacBook-Air.local>
2022-03-10 14:25:19 +0100

    [WIP] Add all pages with minimum content

--- components/Tabbar.tsx
+++ components/Tabbar.tsx
@@ -1,31 +1,31 @@
 import Link from "next/link";
 
 export const TabBar = () => (
-    <div className="tabBar">
-        <Link href="/PageShop">
-            <a className="active">
-                <i className="fa-solid fa-shop"></i>
-                <p>Boutique</p>
-            </a>
-        </Link>
-        <Link href="#">
-            <a>
-                <i className="fa-solid fa-gun"></i>
-                <p>Partie</p>
-            </a>
-        </Link>
-        {/* Page under conception */}
-        {/* <Link href="#">
+  <div className="tabBar">
+    <Link href="/PageShop">
+      <a className="active">
+        <i className="fa-solid fa-shop"></i>
+        <p>Boutique</p>
+      </a>
+    </Link>
+    <Link href="/PageGame">
+      <a>
+        <i className="fa-solid fa-gun"></i>
+        <p>Partie</p>
+      </a>
+    </Link>
+    {/* Page under conception */}
+    {/* <Link href="#">
             <a>
                 <i className="fa-solid fa-screwdriver-wrench"></i>
                 <p>Modding</p>
             </a>
         </Link> */}
-        <Link href="#">
-            <a>
-                <i className="fa-solid fa-at"></i>
-                <p>Contact</p>
-            </a>
-        </Link>
-    </div>
-)
+    <Link href="/PageContact">
+      <a>
+        <i className="fa-solid fa-at"></i>
+        <p>Contact</p>
+      </a>
+    </Link>
+  </div>
+);
---
+++ pages/PageGame.tsx
@@ -0,0 +1,43 @@
+// Import components from Next
+import Head from "next/head";
+
+// Import custom components
+import { Footer, Header, TabBar } from "../components";
+
+// Other imports
+import { clientGraphQL, queryRepliques } from "../utils";
+
+const PageGame = () => {
+  return (
+    <main>
+      <Head>
+        <script src="https://kit.fontawesome.com/ec5d791fc6.js"></script>
+      </Head>
+
+      <Header isHomePage={false} />
+
+      <TabBar />
+
+      <section></section>
+
+      <Footer />
+    </main>
+  );
+};
+
+export async function getStaticProps() {
+  // Log only on server side
+  console.log("This is server side");
+  // Get data from API
+  const data = await clientGraphQL.query({
+    query: queryRepliques,
+  });
+
+  return {
+    props: {
+      articles: data.data.allArticles.edges,
+    },
+  };
+}
+
+export default PageGame;
